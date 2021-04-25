d3f56cc5f944cadad4b18a9b662f4b935e8be7dd
zawad2221 <rifat2221@outlook.com>
2021-04-25 06:48:29 +0600

    update

--- parcel_delivery/settings.py
+++ parcel_delivery/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoice.middleware.WhiteNoiceMiddleware'
+    
 ]
 
 ROOT_URLCONF = 'parcel_delivery.urls'
